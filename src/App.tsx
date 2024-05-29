7b37cbcf3e49a7c9b6689f9634eae49f5f65c512
anaclaraalencar <anaclaraalenc12272gmail.com>
2024-05-29 12:08:18 -0300

    construindo os componentes home, navbar e footer

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navBar/NavBar';
+import Home from './pages/home/home';
+
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Footer/>
+    </>
+   
+  )
+}
+
+export default App;
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,55 @@
+
+function Home() {
+    return (
+        <>
+
+        
+
+            <div className="
+                bg-slate-100
+                flex 
+                justify-center
+                ">
+                <div className='
+                    container 
+                    grid 
+                    grid-cols-2 
+                    text-1000
+                    '>
+                    <div className="
+                        flex 
+                        flex-col 
+                        gap-4 
+                        items-center 
+                        justify-center 
+                        py-4
+                        ">
+                        <h2 className='
+                            text-5xl 
+                            font-bold
+                            '>
+                            Bem vindo à Drogaria Bom Preço!
+                        </h2>
+                        <p className='text-xl'>A drogaria com o precinho mais em conta para você!</p>
+
+                        <div className="flex justify-around gap-4">
+                            
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img
+                            src="https://cdn.discordapp.com/attachments/1213109662677737511/1245346037493661777/HheNdw7.png?ex=66586a60&is=665718e0&hm=7641a1e64f7c1ec77ad1dc6a99ab952844b7bcead4fd781e6a636fcd7299a8b4&"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
+                        />
+                    </div>
+                </div>
+            </div>
+
+            
+        </>
+    )
+}
+
+export default Home
